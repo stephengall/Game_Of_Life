b520da8860e5762d424441dde496c955c8b06c18
Stephen Gallagher <stephengallagher54@gmail.com>
2022-10-18 22:30:41 +0100

    added grid printer + click to draw func

--- Game_Of_Life_Code/sketch.js
+++ Game_Of_Life_Code/sketch.js
@@ -1,7 +1,43 @@
-function setup() {
-  createCanvas(400, 400);
-}
+var scale;
+var grid = [];
 
+function setup() {
+  scale = 35;
+
+  createCanvas(800, 800);
+  for(var i = 0; i < scale; i++){
+    var temp = [];
+    for(var j = 0; j < scale; j++){
+      temp.push(false);
+    }
+    grid.push(temp);
+  }
+}
 function draw() {
-  background(220);
+  background(51);
+
+  checkForClicks();
+  printGrid();
+
+}
+function checkForClicks(){
+  if(mouseIsPressed){
+    var posX = floor(mouseX / width * scale);
+    var posY = floor(mouseY / height * scale);
+    grid[posX][posY] = true;
+  }
+}
+function printGrid(){
+  for(var i = 0; i < scale; i++){
+    for(var j = 0; j < scale; j++){
+      if(grid[i][j]){
+        var xPos = i * (width / scale);
+        var yPos = j * (height / scale);
+        //drawing rectangle
+        noStroke();
+        fill(255);
+        rect(xPos, yPos, width / scale, height / scale);
+      }
+    }
+  }
 }
